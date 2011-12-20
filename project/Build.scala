1d2667857fb8ff3f1a633a8d2446c6f4ac815c9e
Boule Hondevice <boule.hondevice@braldahim.com>
2011-12-20 21:27:58 +0100

    Add dependencies jGit

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+		"org.eclipse.jgit" % "org.eclipse.jgit" % "1.1.0.201109151100-r"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here     
+		resolvers += "jgit-repository" at "http://download.eclipse.org/jgit/maven"
     )
 
 }
